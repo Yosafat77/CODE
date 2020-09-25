cb80a619336192e4d58aff7d7de237d9b29438db
Yosafat77 <71898201+Yosafat77@users.noreply.github.com>
2020-09-25 18:01:50 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,35 +1,23 @@
-//YOSAFAT SANCHEZ VASQUEZ------------25/09/20------------
+//YOSAFAT SANCHEZ VASQUEZ-----------25/09/20-------------
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    float Vs;
-    int R1,R2,R3;
-    float I,RT,REQ;
 
-    printf("Escribe el valor de la fuente Vs: ");
+    float Vs;
+    float VL1=1.5,VL2=1.8,VL3=1.3,I1=0.15;
+    float R1,R2,R3;
+
+    printf("Ingresa el valor de la fuente Vs:");
     scanf("%f",&Vs);
 
-    printf("Restencia 1: ");
-    scanf("%d",&R1);
-    printf("Resistencia 2: ");
-    scanf("%d",&R2);
-    printf("Resistencia 3: ");
-    scanf("%d",&R3);
+    R1=(Vs-VL1)/I1;
+    R2=(Vs-VL2)/I1;
+    R3=(Vs-VL3)/I1;
 
-
-
-     RT=((R2*R3)/(R2+R3)); //simplificamos circuito con R3 Y R2
-    REQ=((RT*R1)/(RT+R1));//resistencia total con RT yR1
-    I=Vs/REQ; //Corriente
-    printf("La corriente I es %.2fA\n",I);
-    printf("El voltaje en R1 (%d ohms) es: %0.2fV\n",R1,I*R1);
-    printf("El voltaje en R2 (%d ohms) es: %0.2fV\n",R2,I*R2);
-    printf("El voltaje en R3 (%d ohms) es: %0.2fV\n",R3,I*R3);
-
-    printf("La corriente en R1 (%d ohms) es: %0.2fI\n",R1,Vs/R1);
-    printf("La corriente en R2 (%d ohms) es: %0.2fI\n",R2,Vs/R2);
-    printf("La corriente en R3 (%d ohms) es: %0.2fI\n",R3,Vs/R3);
-return 0;
-
+    printf("La resistencia para LED rojo es: %0.2fohms\n\n",R1);
+    printf("La resistencia para LED verde es: %0.2fohms\n\n",R2);
+    printf("La resistencia para LED amarillo es: %0.2fohms\n\n",R3);
+    return 0;
 }
